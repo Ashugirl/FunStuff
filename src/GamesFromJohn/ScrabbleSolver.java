3ed6bfa74390f13cdfaa14b296680305e2397481
Aviva Shuman <97950104+Ashugirl@users.noreply.github.com>
2022-07-04 14:17:30 +0200

    Add files via upload

--- src/GamesFromJohn/ScrabbleSolver.java
+++ src/GamesFromJohn/ScrabbleSolver.java
@@ -12,13 +12,33 @@ public class ScrabbleSolver {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your letters");
-        String letters = scanner.nextLine();
+        String letters = scanner.nextLine().toUpperCase();
 
+        Map<Character, Integer> lettersCountMap =  getCharacterCountMap(letters);
 
 
         BufferedReader reader = new BufferedReader(new FileReader("/Users/avivashuman/Desktop/scrabbleDictionary.txt"));
+        System.out.println("You can make these words with your letters: " );
+
         for(String currentWord = reader.readLine(); currentWord != null; currentWord = reader.readLine()){
-            System.out.println(currentWord);
+            Map<Character, Integer> currentWordMap = getCharacterCountMap(currentWord);
+
+            boolean canMakeCurrentWord = true;
+            for(Character character : currentWordMap.keySet()){
+                int currentWordCharCount = currentWordMap.get(character);
+                int lettersCharCount = lettersCountMap.containsKey(character) ?
+                        lettersCountMap.get(character)  : 0;
+
+                if(currentWordCharCount > lettersCharCount){
+                    canMakeCurrentWord = false;
+                    break;
+                }
+            }
+            if(canMakeCurrentWord){
+                System.out.println(currentWord);
+            }
+            scanner.close();
+            //reader.close();
         }
 
 
